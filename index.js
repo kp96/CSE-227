dc8f27cd44f2483211c141907f2ed7a548809eb8
Krishna Kalubandi <krishnakalubandi@gmail.com>
2017-11-22 11:33:36 -0800

    refresh bug data

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var gitapi = require('./lib/gitapi'),
     bugs = JSON.parse(require('fs').readFileSync('./data/security_buglist_large.json')).bugs,
     _ = require('lodash'),
     async = require('async'),
-    bugIds = _.slice(_.map(bugs, 'id'), 0, 200);
 
-async.mapLimit(bugIds, 5, function(bugId, cb) {
+    buggys = _.map(bugs, 'id');
+
+var bugIds = _.slice(buggys, _.indexOf(buggys, 202198), 2000);
+
+console.log(_.indexOf(buggys, 315871));
+
+async.mapLimit(bugIds, 3, function(bugId, cb) {
     gitapi.searchAndProcessBug(bugId, function(err) {
         if (err) {
             console.warn(err);
